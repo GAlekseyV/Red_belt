5dbaa1c5db78d305776b91a4677bfa4f980f220c
Alexey Guzenkov <galekseyv@gmail.com>
2018-09-28 09:43:36 +0300

    Week 3, task 1 pass

--- Week_3/task_1_objectPool/object_pool.cpp
+++ Week_3/task_1_objectPool/object_pool.cpp
@@ -6,20 +6,73 @@
 #include <queue>
 #include <stdexcept>
 #include <set>
+
 using namespace std;
 
 template <class T>
 class ObjectPool {
 public:
-  T* Allocate();
-  T* TryAllocate();
+  T* Allocate(){
+    // Если есть хотя бы один освобожденный объект, перености его
+    // во множество выделенных и возвращает указатель.
+    // Если освобожденных объектов нет, создает новый объект и помещает
+    // его во множество выделенных, возвращает указатель на него
+    T* new_object;
+    if(!free_objects.empty()){
+      new_object = free_objects.front();
+      selected_objects.insert(free_objects.front());
+      free_objects.pop();
+    }else{
+      new_object = new T;
+      selected_objects.insert(new_object);
+    }
+    return new_object;
+  }
 
-  void Deallocate(T* object);
+  T* TryAllocate(){
+    // Работает аналогично Allocate, если освобожденных объектов нет
+    // возвращает nullptr;
+    T* new_object;
+    if(!free_objects.empty()){
+      new_object = free_objects.front();
+      selected_objects.insert(free_objects.front());
+      free_objects.pop();
+    }else{
+      new_object = nullptr;
+    }
+    return new_object;
+  }
 
-  ~ObjectPool();
+  void Deallocate(T* object){
+    // Переносит объект из множества выделенных в множество освобождённых;
+    // если переданный объект не содержится в множестве выделенных, метод Deallocate
+    // должен бросать исключение invalid_argument.
+    if(selected_objects.count(object) != 0){
+      free_objects.push(object);
+      selected_objects.erase(object);
+    }else{
+      throw invalid_argument("Object is not find");
+    }
+  }
+
+
+  ~ObjectPool(){
+    while(!selected_objects.empty()){
+      auto deallocate_object = *selected_objects.begin();
+      Deallocate(deallocate_object);
+    }
+    while(!free_objects.empty()){
+      T* deleting_object = free_objects.front();
+      free_objects.pop(); // Delete from queue and call destructor
+      delete deleting_object;
+    }
+  }
 
 private:
   // Добавьте сюда поля
+  // Два набора объектов: выделенные и освобожденные
+  set<T*> selected_objects;
+  queue<T*> free_objects;
 };
 
 void TestObjectPool() {
