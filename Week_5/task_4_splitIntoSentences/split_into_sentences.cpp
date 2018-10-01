f8936ceab06f03d5e471d7eb37b6f21d25ecba83
Alexey Guzenkov <galekseyv@gmail.com>
2018-10-01 19:53:49 +0300

    Week 5. Task 4. Check solution

--- Week_5/task_4_splitIntoSentences/split_into_sentences.cpp
+++ Week_5/task_4_splitIntoSentences/split_into_sentences.cpp
@@ -1,5 +1,6 @@
 #include "test_runner.h"
 
+#include <algorithm>
 #include <vector>
 
 using namespace std;
@@ -12,30 +13,35 @@ using namespace std;
 template <typename Token>
 using Sentence = vector<Token>;
 
+template <typename TokenForwardIt>
+TokenForwardIt FindSentenceEnd(TokenForwardIt tokens_begin, TokenForwardIt tokens_end) {
+  const TokenForwardIt before_sentence_end =
+          adjacent_find(tokens_begin, tokens_end,
+                        [](const auto& left_token, const auto& right_token) {
+                            return left_token.IsEndSentencePunctuation()
+                                   && !right_token.IsEndSentencePunctuation();
+                        });
+  return before_sentence_end == tokens_end
+         ? tokens_end
+         : next(before_sentence_end);
+}
+
 // Класс Token имеет метод bool IsEndSentencePunctuation() const
 template <typename Token>
 vector<Sentence<Token>> SplitIntoSentences(vector<Token> tokens) {
-  // Напишите реализацию функции, не копируя объекты типа Token
   vector<Sentence<Token>> sentences;
-  vector<Token> sentence;
-  bool isPrevTokenStatus = false;
-  auto startSentence = tokens.begin();
-  for(auto it = tokens.begin(); it != tokens.end(); ++it){
-    if(isPrevTokenStatus && !it->IsEndSentencePunctuation()){
-      move(startSentence, it, back_inserter(sentence));
-      sentences.push_back(move(sentence));
-      startSentence = it;
+  auto tokens_begin = begin(tokens);
+  const auto tokens_end = end(tokens);
+  while (tokens_begin != tokens_end) {
+    const auto sentence_end = FindSentenceEnd(tokens_begin, tokens_end);
+    Sentence<Token> sentence;
+    for (; tokens_begin != sentence_end; ++tokens_begin) {
+      sentence.push_back(move(*tokens_begin));
     }
-    isPrevTokenStatus = it->IsEndSentencePunctuation();
-  }
-  if(startSentence != tokens.end())
-  {
-    move(startSentence, tokens.end(), back_inserter(sentence));
     sentences.push_back(move(sentence));
   }
-  return move(sentences);
+  return sentences;
 }
-
 
 struct TestToken {
   string data;
