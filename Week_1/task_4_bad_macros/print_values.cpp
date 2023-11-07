9b0ca5ca7274a88873955c288f99e944757964c2
GAlekseyV <galekseyv@gmail.com>
2023-11-07 19:40:17 +0300

    Add descriptions for macro tasks.

--- Week_1/task_4_bad_macros/print_values.cpp
+++ Week_1/task_4_bad_macros/print_values.cpp
@@ -1,16 +1,20 @@
 #include "test_runner.h"
 
 #include <ostream>
+#include <sstream>
 using namespace std;
 
 #define PRINT_VALUES(out, x, y) \
-out << (x) << '\n' << (y) << endl
+  (out) << (x) << '\n'          \
+        << (y) << '\n'
 
-int main() {
-    TestRunner tr;
-    tr.RunTest([] {
-        ostringstream output;
-        PRINT_VALUES(output, 5, "red belt");
-        ASSERT_EQUAL(output.str(), "5\nred belt\n");
-    }, "PRINT_VALUES usage example");
+int main()
+{
+  TestRunner tr;
+  tr.RunTest([] {
+    ostringstream output;
+    PRINT_VALUES(output, 5, "red belt");
+    ASSERT_EQUAL(output.str(), "5\nred belt\n");
+  },
+    "PRINT_VALUES usage example");
 }
--- Week_1/task_5_uniq_id/uniq_id.cpp
+++ Week_1/task_5_uniq_id/uniq_id.cpp
@@ -3,13 +3,14 @@
 using namespace std;
 
 // Реализуйте этот макрос так, чтобы функция main компилировалась
-#define ID(line) var_ ## line
+#define ID(line) var_##line
 #define CREATE(line) ID(line)
 #define UNIQ_ID CREATE(__LINE__)
 
-int main() {
+int main()
+{
   int UNIQ_ID = 0;
   string UNIQ_ID = "hello";
-  vector<string> UNIQ_ID = {"hello", "world"};
-  vector<int> UNIQ_ID = {1, 2, 3, 4};
+  vector<string> UNIQ_ID = { "hello", "world" };
+  vector<int> UNIQ_ID = { 1, 2, 3, 4 };
 }
