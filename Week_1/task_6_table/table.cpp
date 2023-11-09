329db5c68e94d52643e16b4dffada027e14ac581
GAlekseyV <galekseyv@gmail.com>
2023-11-09 21:16:53 +0300

    Update the solution of the task 6 of week 1.

--- Week_1/task_6_table/table.cpp
+++ Week_1/task_6_table/table.cpp
@@ -1,50 +1,61 @@
 #include "test_runner.h"
 
-#include <vector>
+#include <cstddef>
 #include <utility>
+#include <vector>
 
 using namespace std;
 
 // Реализуйте здесь шаблонный класс Table
 template<typename T>
-class Table{
+class Table
+{
 public:
-    Table(size_t rows, size_t columns){
-        Resize(rows, columns);
+  Table(size_t rows, size_t columns)
+  {
+    Resize(rows, columns);
+  }
+  vector<T> &operator[](size_t index)
+  {
+    return table[index];
+  }
+  const vector<T> &operator[](size_t index) const
+  {
+    return table[index];
+  }
+  void Resize(size_t new_rows, size_t new_columns)
+  {
+    table.resize(new_rows);
+    for (auto &row : table) {
+      row.resize(new_columns);
     }
-    vector<T>& operator[](size_t index){
-        return table[index];
-    }
-    const vector<T>& operator[](size_t index) const{
-        return table[index];
-    }
-    void Resize(size_t new_rows, size_t new_columns){
-        table.resize(new_rows);
-        for(auto& r : table){
-            r.resize(new_columns);
-        }
-    }
-    pair<size_t, size_t> Size()const{
-        return make_pair(table.size(), (table.empty()? 0 : table[0].size()));
-    }
+  }
+  [[nodiscard]] pair<size_t, size_t> Size() const
+  {
+    return make_pair(table.size(), (table.empty() ? 0 : table[0].size()));
+  }
+
 private:
-    vector<vector<T>> table;
+  vector<vector<T>> table;
 };
 
 
-void TestTable() {
-  Table<int> t(1, 1);
-  ASSERT_EQUAL(t.Size().first, 1u);
-  ASSERT_EQUAL(t.Size().second, 1u);
-  t[0][0] = 42;
-  ASSERT_EQUAL(t[0][0], 42);
-  t.Resize(3, 4);
-  ASSERT_EQUAL(t.Size().first, 3u);
-  ASSERT_EQUAL(t.Size().second, 4u);
+void TestTable()
+{
+  Table<int> tbl(1, 1);
+  ASSERT_EQUAL(tbl.Size().first, 1U);
+  ASSERT_EQUAL(tbl.Size().second, 1U);
+  const int test_data = 42;
+  tbl[0][0] = test_data;
+  ASSERT_EQUAL(tbl[0][0], test_data);
+  tbl.Resize(3, 4);
+  ASSERT_EQUAL(tbl.Size().first, 3U);
+  ASSERT_EQUAL(tbl.Size().second, 4U);
 }
 
-int main() {
-  TestRunner tr;
-  RUN_TEST(tr, TestTable);
+int main()
+{
+  TestRunner trunner;
+  RUN_TEST(trunner, TestTable);
   return 0;
 }
