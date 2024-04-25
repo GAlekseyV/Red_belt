19f438c55fac14cd2f3a7cdd19378ed7d93310e0
GAlekseyV <galekseyv@gmail.com>
2024-04-25 19:44:17 +0300

    Add the solution of the task 7 week 5.

--- Week_5/task_7_priority_container/priority_collection_2.cpp
+++ Week_5/task_7_priority_container/priority_collection_2.cpp
@@ -1,114 +1,99 @@
 #include "test_runner.h"
 #include <algorithm>
-#include <iostream>
+#include <cstddef>
 #include <iterator>
-#include <memory>
 #include <set>
+#include <string>
 #include <utility>
 #include <vector>
-#include <list>
 
 using namespace std;
 
-template <typename T>
-class PriorityCollection {
+template<typename T>
+class PriorityCollection
+{
 public:
   /* тип, используемый для идентификаторов */
-  using Id = typename set<T>::iterator;
+  using Id = size_t;
 
   // Добавить объект с нулевым приоритетом
   // с помощью перемещения и вернуть его идентификатор
-  Id Add(T object) {
-    auto id = objects.insert(std::move(object)).first;
-    if (priority.size() == 0) {
-      priority.push_back(list<Id>());
-    }
-      priority[0].push_back(id);
-    return id;
+  Id Add(T object)
+  {
+    objects.emplace_back(move(object), 0);
+    sorted_objects.emplace(0, last_id);
+    return Id(last_id++);
   }
 
   // Добавить все элементы диапазона [range_begin, range_end)
   // с помощью перемещения, записав выданные им идентификаторы
   // в диапазон [ids_begin, ...)
-  template <typename ObjInputIt, typename IdOutputIt>
-  void Add(ObjInputIt range_begin, ObjInputIt range_end,
-           IdOutputIt ids_begin);
+  template<typename ObjInputIt, typename IdOutputIt>
+  void Add(ObjInputIt range_begin, ObjInputIt range_end, IdOutputIt ids_begin)
+  {
+    while (range_begin != range_end) {
+      *ids_begin++ = Add(move(*range_begin++));
+    }
+  }
 
   // Определить, принадлежит ли идентификатор какому-либо
   // хранящемуся в контейнере объекту
-  bool IsValid(Id id) const {
-    return objects.count(*id) > 0;
+  [[nodiscard]] bool IsValid(Id id) const
+  {
+    return objects[id].second >= 0;
   }
 
   // Получить объект по идентификатору
-  const T& Get(Id id) const {
-      if (IsValid(id)) {
-      return (*id);
-    }
-      return {};
+  [[nodiscard]] const T &Get(Id id) const
+  {
+    return objects[id].first;
   }
 
   // Увеличить приоритет объекта на 1
-  void Promote(Id id) {
-    pair<Id, int> p = find(id);
-    if (p.second < (priority.size() - 1)) {
-      priority[p.second + 1].push_back(p.first);
-    } else {
-      priority.push_back(list<Id>());
-      priority.back().push_back(id);
-    }
-    priority[p.second].remove(p.first);
+  void Promote(Id id)
+  {
+    auto old_priority = objects[id].second++;
+    auto new_priority = old_priority + 1;
+    sorted_objects.erase({ old_priority, id });
+    sorted_objects.emplace(new_priority, id);
   }
 
   // Получить объект с максимальным приоритетом и его приоритет
   pair<const T &, int> GetMax() const
   {
-    int p = priority.size() - 1;
-    auto it = priority.back().back();
-    return make_pair(move(*it, p);
+    return objects[prev(sorted_objects.end())->second];
   };
 
   // Аналогично GetMax, но удаляет элемент из контейнера
-  pair<T, int> PopMax(){
-    int p = priority.size() - 1;
-    while (priority[p].size() < 1) {
-      p--;
-    }
-    auto it = priority[p].back();
-    priority[p].pop_back();
-    return make_pair(move(objects.extract(it).value()), p);
+  pair<T, int> PopMax()
+  {
+    auto id = prev(sorted_objects.end())->second;
+    auto result = move(objects[id]);
+    sorted_objects.erase({ result.second, id });
+    objects[id] = make_pair(T(), -1);
+    return result;
   };
 
 private:
-    pair<Id, int> find(Id id) {
-      //assert(IsValid(id));
-      int i = 0;
-      typename std::list<Id>::iterator it;
-      for (; i < priority.size(); i++) {
-        it = std::find(priority[i].begin(), priority[i].end(), id);
-        
-        if (it != priority[i].end()) {
-          break;
-        }
-      }
-      return { *it, i };
-  }
   // Приватные поля и методы
-  set<T> objects;
-  vector<list<Id>> priority;
+  set<pair<int, Id>> sorted_objects;
+  vector<std::pair<T, int>> objects;
+  size_t last_id = 0;
 };
 
 
-class StringNonCopyable : public string {
+class StringNonCopyable : public string
+{
 public:
-  using string::string;  // Позволяет использовать конструкторы строки
-  StringNonCopyable(const StringNonCopyable&) = delete;
-  StringNonCopyable(StringNonCopyable&&) = default;
-  StringNonCopyable& operator=(const StringNonCopyable&) = delete;
-  StringNonCopyable& operator=(StringNonCopyable&&) = default;
+  using string::string;// Позволяет использовать конструкторы строки
+  StringNonCopyable(const StringNonCopyable &) = delete;
+  StringNonCopyable(StringNonCopyable &&) = default;
+  StringNonCopyable &operator=(const StringNonCopyable &) = delete;
+  StringNonCopyable &operator=(StringNonCopyable &&) = default;
 };
 
-void TestNoCopy() {
+void TestNoCopy()
+{
   PriorityCollection<StringNonCopyable> strings;
   const auto white_id = strings.Add("white");
   const auto yellow_id = strings.Add("yellow");
@@ -136,7 +121,8 @@ void TestNoCopy() {
   }
 }
 
-int main() {
+int main()
+{
   TestRunner tr;
   RUN_TEST(tr, TestNoCopy);
   return 0;
